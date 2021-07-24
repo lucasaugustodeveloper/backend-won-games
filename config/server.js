7eafe275524d289d94f01fd647865531bc6f0c8d
Lucas Augusto <lucas.augusto5061@gmail.com>
2021-07-24 13:32:47 -0300

    started project won games

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dea20adc403e9bac96f867d293200411'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '59929c3c-ccd7-43b3-a0ad-4527cfe93a02'
+};
